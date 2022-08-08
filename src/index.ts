1789e8bcf9ab0f8a1853489c102dabc23d87a4dc
ghostebony <ghostebony@proton.me>
2022-08-08 20:21:47 -0300

    restructure project

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,85 @@
 import archiver from "archiver";
 import { createReadStream, createWriteStream, existsSync } from "fs";
-import { access, mkdir, readdir, rm, unlink, writeFile } from "fs/promises";
+import { access, mkdir, readdir, rm, rmdir, unlink, writeFile } from "fs/promises";
 import { sep } from "path";
 import type { Stream } from "stream";
 import { createGunzip, createGzip } from "zlib";
 
-export const downloadFile = async (url: string, folderPath: string) => {
-	const fileName = url.split(sep).pop() || "unknown.file";
-
-	const filePath = await createFile(
-		folderPath,
-		fileName,
-		(await (await fetch(url)).blob()).stream()
-	);
-
-	return { fileName, filePath };
-};
-
-export const createFolder = async (path: string, recursive: boolean = false) => {
-	if (!existsSync(path)) await mkdir(path, { recursive });
-};
-
-export const createFile = async (path: string, fileName: string, data: string | Stream) => {
+const file_create = async (path: string, fileName: string, data: string | Stream) => {
 	const filePath = path + sep + fileName;
 
-	await createFolder(path);
+	await folder.create(path);
 
 	await writeFile(filePath, data);
 
 	return filePath;
 };
 
-export const fileExists = async (filePath: string) => {
-	try {
-		await access(filePath);
-		return true;
-	} catch {
-		return false;
-	}
+export const file = {
+	async download(url: string, folderPath: string) {
+		const fileName = url.split(sep).pop() || "unknown.file";
+
+		const filePath = await file_create(
+			folderPath,
+			fileName,
+			(await (await fetch(url)).blob()).stream()
+		);
+
+		return { fileName, filePath };
+	},
+	async exists(filePath: string) {
+		try {
+			await access(filePath);
+			return true;
+		} catch {
+			return false;
+		}
+	},
+	create: file_create,
+	async delete(filePath: string) {
+		try {
+			await unlink(filePath);
+			return true;
+		} catch {
+			return false;
+		}
+	},
+	gzip(filePath: string) {
+		return createReadStream(filePath)
+			.pipe(createGzip())
+			.pipe(createWriteStream(filePath + ".gz"));
+	},
+	gunzip(filePath: string) {
+		return createReadStream(filePath)
+			.pipe(createGunzip())
+			.pipe(createWriteStream(filePath.split(".").slice(0, -1).join(".")));
+	},
 };
 
-export const deleteFile = async (filePath: string) => {
-	try {
-		await unlink(filePath);
-		return true;
-	} catch {
-		return false;
-	}
+export const folder = {
+	async create(path: string, recursive: boolean = false) {
+		if (!existsSync(path)) await mkdir(path, { recursive });
+	},
+	async delete(path: string, recursive: boolean = false) {
+		return rmdir(path, { recursive });
+	},
+	async deleteFiles(path: string) {
+		return Promise.allSettled(
+			(await readdir(path)).map((fileName) => file.delete(path + sep + fileName))
+		);
+	},
+	async deleteWithFiles(path: string, recursive: boolean = false) {
+		return rm(path, { recursive });
+	},
+	async gzip(filePath: string, folderPath: string) {
+		const archive = archiver("tar", { gzip: true });
+
+		archive.pipe(createWriteStream(filePath));
+
+		archive.directory(folderPath, false);
+
+		await archive.finalize();
+	},
 };
 
-export const deleteFolder = async (path: string, recursive: boolean = false) =>
-	rm(path, { recursive });
-
-export const deleteFolderFiles = async (path: string) =>
-	Promise.allSettled((await readdir(path)).map((fileName) => deleteFile(path + sep + fileName)));
-
-export const gzipFolder = async (filePath: string, folderPath: string) => {
-	const archive = archiver("tar", { gzip: true });
-
-	archive.pipe(createWriteStream(filePath));
-
-	archive.directory(folderPath, false);
-
-	await archive.finalize();
-};
-
-export const gzipFile = (filePath: string) =>
-	createReadStream(filePath)
-		.pipe(createGzip())
-		.pipe(createWriteStream(filePath + ".gz"));
-
-export const gunzipFile = (filePath: string) =>
-	createReadStream(filePath)
-		.pipe(createGunzip())
-		.pipe(createWriteStream(filePath.split(".").slice(0, -1).join(".")));
+export default { file, folder };
