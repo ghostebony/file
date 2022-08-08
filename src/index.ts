6a6963ae932646f972834a5a01669df8bca62cb0
ghostebony <ghostebony@proton.me>
2022-08-08 17:53:41 -0300

    add <fileExists> function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { existsSync } from "fs";
-import { mkdir } from "fs/promises";
+import { access, mkdir } from "fs/promises";
 
 export const createFolder = async (path: string, recursive: boolean = false) => {
 	if (!existsSync(path)) await mkdir(path, { recursive });
+};
+
+export const fileExists = async (filePath: string) => {
+	try {
+		await access(filePath);
+		return true;
+	} catch {
+		return false;
+	}
 };
